1b9690d346a55690f1cc2974bdcb4617cf3142b2
sutaojie <sutaojie@gmail.com>
2018-07-11 23:36:39 +0800

    step-3

---
+++ step-3/rollup.config.js
@@ -0,0 +1,10 @@
+const replace = require('rollup-plugin-replace')
+
+rollup({
+  // ...
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
+}).then(...)
---
+++ step-3/src/index.js
@@ -0,0 +1,30 @@
+import bar from './bar';
+import Vue from 'vue'
+var app = new Vue({
+  el: '#app',
+   data: {
+  	newTodo:'',
+  	todoList:[]
+  },
+  created(){
+  	window.onbeforeunload=()=>{
+  		let dataString = JSON.stringify(this.todoList)
+  		window.localStorage.setItem('myTodos', dataString)
+  	}
+  	let oldDataString = window.localStorage.getItem('myTodos')
+  	let oldData = JSON.parse(oldDataString)
+  	this.todoList = oldData || []
+  }
+
+  ,
+  methods:{
+  	addTodo:function(){
+  		this.todoList.push({title: this.newTodo, createAt: new Date(), done: false})
+  		this.newTodo = ''
+	 },
+	 removeTode(todo){
+	 	let index = this.todoList.indexOf(todo)
+	 	this.todoList.splice(index, 1)
+	 }
+  },
+})
